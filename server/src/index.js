1d9ff42b3e3d3f7db5d5055d851e2ce3985ff994
rzywczak <73279676+rzywczak@users.noreply.github.com>
2022-04-25 13:27:47 +0200

    env example change / src/index.js to development

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 require('./db/mongoose')
-const history = require('connect-history-api-fallback');
+// const history = require('connect-history-api-fallback');
 const userRouter = require('./routers/user')
 const tankRouter = require('./routers/tank')
 const cors = require('cors')
@@ -14,13 +14,13 @@ app.use(express.json())
 app.use(userRouter)
 app.use(tankRouter)
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(history());
-    app.use(express.static(__dirname + '/public/'));
-    app.get('/', function(req, res) {
-     res.sendFile(__dirname+'/public/index.html');
-    })
-}
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(history());
+//     app.use(express.static(__dirname + '/public/'));
+//     app.get('/', function(req, res) {
+//      res.sendFile(__dirname+'/public/index.html');
+//     })
+// }
 
 app.listen(port, () => {
     console.log('Server is up on port '+ port)
